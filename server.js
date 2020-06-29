5184a7d9715ac58de62862d5956463b7b90f5644
Christine Clemens <christineclemens.tesol@gmail.com>
2020-06-29 17:26:03 -0400

    Replace comments.

--- server.js
+++ server.js
@@ -31,18 +31,18 @@ app.get("*", function(req, res) {
     res.redirect("/")
 });
 
-//* GET `/api/notes` - Should read the `db.json` file and return all saved notes as JSON.
+//Restful API get request that cycles through iterable note entries and assigns each a unique id.
 app.get("/api/notes", function(req, res) {
     for (const [id,note] of notesInput.entries()){
     note.id = id + 1
 };
 
-//* POST `/api/notes` - Should receive a new note to save on the request body, add it to the `db.json` file, and then return the new note to the client.
+//Restful API post request that pushes a new note entry to the request body.
 app.post("/api/notes", function(req, res) {
     notesInput.push(req.body)
 });
 
-//* DELETE `/api/notes/:id` - Should receive a query parameter containing the id of a note to delete. This means you'll need to find a way to give each note a unique `id` when it's saved. In order to delete a note, you'll need to read all notes from the `db.json` file, remove the note with the given `id` property, and then rewrite the notes to the `db.json` file.
+//Restful API delete request that separates saved notes to remove a single entry and its id.
 app.delete("api/notes/:id", function (req, res) {
     notesInput.splice(req.params.id -1, 1)
 });
