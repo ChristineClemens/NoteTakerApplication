2dead887575f2176a4ac7383dc3257837d72cbe7
Christine Clemens <christineclemens.tesol@gmail.com>
2020-06-29 12:39:37 -0400

    Added API get requests that return notes.html and index.html files.

--- server.js
+++ server.js
@@ -17,3 +17,13 @@ app.use(express.urlencoded({extended:true}));
 app.listen(PORT, function() {
     console.log(`App listening on PORT ${PORT}`);
 });
+
+//Restful API get request that returns notes.html.
+app.get("/notes", function(req, res) {
+    res.redirect("/notes.html")
+});
+
+//Restful API get request that returns index.html.
+app.get("*", function(req, res) {
+    res.redirect("/")
+});
