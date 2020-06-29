68f49a382479c2ed7f15e556d3b75c1b8c1c2caa
Christine Clemens <christineclemens.tesol@gmail.com>
2020-06-29 12:24:14 -0400

    Added server.js file and intialized setup.

---
+++ server.js
@@ -0,0 +1,19 @@
+//Define required modules.
+const express = require ("express");
+const fs = require ("fs");
+
+//Specify the web application framework.
+const app = express();
+
+//Specify the PORT used for this application.
+const PORT = process.env.PORT || 8080;
+
+//Setup to enable Express to handle data parsing.
+app.use(express.json());
+app.use(express.static("public"));
+app.use(express.urlencoded({extended:true}));
+
+//Add a listener to the specified port.
+app.listen(PORT, function() {
+    console.log(`App listening on PORT ${PORT}`);
+});
